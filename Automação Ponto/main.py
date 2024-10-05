adfaac5a0117e9f3f7f4bbba09c8fecec5c9e4a0
Gustavo Moreira <116241079+GuMoreira222@users.noreply.github.com>
2024-10-05 12:00:17 -0300

    Update main.py

--- Automação Ponto/main.py
+++ Automação Ponto/main.py
@@ -1,10 +1,10 @@
-from lib.tratar_env import arquivo_vazio, buscar_file_path, arquivo_existe
+from lib.tratar_env import arquivo_vazio, buscar_file_path, criar_arquivo
 from lib.interface import criar_interface
 from lib.automacao import iniciar_automacao
 
 def main():
     caminho = buscar_file_path()
-    arquivo_existe(caminho)
+    criar_arquivo(caminho)
 
     if arquivo_vazio(caminho):
         criar_interface()
